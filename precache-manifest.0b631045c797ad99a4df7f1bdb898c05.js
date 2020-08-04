3fe8b4af88f188bc78d64a8de9f80faff1ac67f3
Delonge12345 <lobkov1997@gmail.com>
2020-08-04 23:20:14 +0300

    Updates

---
+++ precache-manifest.0b631045c797ad99a4df7f1bdb898c05.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d68634c391f7251b2ac08cf8a57ef8bb",
+    "url": "/SocialNetwork/index.html"
+  },
+  {
+    "revision": "75588d54c14015b3b94e",
+    "url": "/SocialNetwork/static/css/main.e28a3048.chunk.css"
+  },
+  {
+    "revision": "bbf1e0747dd3de2d2c2f",
+    "url": "/SocialNetwork/static/js/2.f9091a05.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SocialNetwork/static/js/2.f9091a05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75588d54c14015b3b94e",
+    "url": "/SocialNetwork/static/js/main.9e0c810f.chunk.js"
+  },
+  {
+    "revision": "4971738a83f029a48280",
+    "url": "/SocialNetwork/static/js/runtime-main.a53948fc.js"
+  },
+  {
+    "revision": "57cba6c6b4c424615f919ae07c86424d",
+    "url": "/SocialNetwork/static/media/1.57cba6c6.png"
+  },
+  {
+    "revision": "51c299c19d84eb3746906f071567d93b",
+    "url": "/SocialNetwork/static/media/exit.51c299c1.png"
+  },
+  {
+    "revision": "5fbf3c12e19c7af15dff5e8f236f3986",
+    "url": "/SocialNetwork/static/media/preloader.5fbf3c12.gif"
+  }
+]);
